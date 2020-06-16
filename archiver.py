7cadc33c63eff15ba660fd75c69cde2cbbb8d85c
Jonas Maurus <jonas.maurus@optile.net>
2020-06-16 16:25:31 +0200

    parse environment variables into integers

--- archiver.py
+++ archiver.py
@@ -7,7 +7,7 @@ class Archiver(executor.Executor):
 
     def archive(self):
         self.logger.info("Archiving")
-        self.ds.safe_archive_all(self.config.archive_threshold)
+        self.ds.safe_archive_all(int(self.config.archive_threshold))
 
 
 if __name__ == "__main__":
